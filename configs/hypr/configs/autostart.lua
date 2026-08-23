e96955690dc8afeea40359405e2dd22fc9339af1
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2026-08-24 01:20:17 +0530

    Fix Incorrect Path

--- configs/hypr/configs/autostart.lua
+++ configs/hypr/configs/autostart.lua
@@ -7,7 +7,7 @@ hl.on("hyprland.start", function()
     -- hl.exec_cmd("systemctl --user start hyprpolkitagent")
 
     -- hl.exec_cmd("waybar")
-    hl.exec_cmd("qs")
+    hl.exec_cmd("qs -c ii")
     hl.exec_cmd("awww-daemon")
 
     hl.exec_cmd("wl-paste --watch cliphist store")
